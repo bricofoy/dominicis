8f23945e3b6b1086df27e36303359446e9e5a47a
Yann Jautard <bricofoy@free.fr>
2020-03-10 15:02:54 +0100

    correction de formattage et ajout de commentaires

--- mesures.ino
+++ mesures.ino
@@ -3,7 +3,7 @@
 struct info_t {
   bool init6 : 1;
   uint8_t index6 : 3;
-  bool init24 : 1;    
+  bool init24 : 1;
   uint8_t index24 : 5;
 } info;
 
@@ -23,7 +23,6 @@ void mesures_purge()
       mesures.next(mesures_attenteAquisition);
       dsChau.requestTemperatures();
     }
-    
 }
 
 void mesures_attenteAquisition()
@@ -35,34 +34,24 @@ void mesures_attenteAquisition()
 void mesures_lectureCapteurs()
 {
   uint8_t i = mesures.runCount();
-//   bool erreur = false;
   
   if (i < sizeof(pinDHT) ) {
-    if (dht.read2(pinDHT[i], &T[i], &H[i], NULL) != SimpleDHTErrSuccess) {
-      T[i] = H[i] = ERREUR;
-//       erreur = true;
-    }
+    if (dht.read2(pinDHT[i], &T[i], &H[i], NULL) != SimpleDHTErrSuccess) T[i] = H[i] = ERREUR;
   }
   else {
     T[i]=dsChau.getTempCByIndex(0);
-/*    if(erreur || (T[i]==ERREUR)) mesures.next(mesures_purge);
-    else */mesures.next(mesures_majMoyennes);
+    mesures.next(mesures_majMoyennes);
   }
 }
 
 void mesures_majMoyennes()
 {
-
-  
   static time_t  derniereFois = 0;
-
-  
   if((now()-derniereFois) < (10 * 60)) {
     mesures.next(mesures_attente);
     return; //on ne met à jour les moyennes que toutes les 10 minutes
   }
   
-
   if(T[DHext]==ERREUR) {
     mesures.next(mesures_purge);//on a besoin d'une valeur correcte pour maj les moyennes
     return;
@@ -73,15 +62,13 @@ void mesures_majMoyennes()
   for(uint8_t i=0;i<6;i++) Tmoy6ext+=tab6Text[i];
   Tmoy6ext/=(float)info.init6?6:(info.index6+1);
   Tmoy6ext/=10;
-
   
   if(++info.index6==6){
     info.init6=true;
     info.index6=0;
   }
   
-  
-  if(info.index6==1) {
+  if(info.index6==1) { //==1 et non 0 pour ne commencer à recopier dans le tableau des 24 éléments que lorsqu'il y a au moins une valeur dans celui de 6, ça évite de démarrer la moyenne avec un zéro
     tab24Text[info.index24]=round(Tmoy6ext*10);
     Tmoy24ext=0;
     for(uint8_t i=0;i<24;i++) Tmoy24ext+=tab24Text[i];
